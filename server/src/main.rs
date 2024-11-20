787b83c9b2a8a9cf0dfd7347669858902a390547
&nbsp <40737538+crnvl@users.noreply.github.com>
2024-11-20 22:48:39 +0100

    feat: create raw openai request

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,7 +1,11 @@
 use actix_web::{App, HttpServer};
 
+mod http;
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    dotenv::dotenv().ok();
+
     HttpServer::new(|| App::new())
         .bind(("127.0.0.1", 8080))?
         .run()
