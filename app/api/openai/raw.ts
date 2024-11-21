0339972460c0ac6e5bb83645996295c256c2064d
&nbsp <40737538+crnvl@users.noreply.github.com>
2024-11-21 23:11:29 +0100

    feat: adjust eslint rules

--- app/api/openai/raw.ts
+++ app/api/openai/raw.ts
@@ -3,8 +3,8 @@ import { NextApiRequest, NextApiResponse } from "next";
 import OpenAI from "openai";
 const openai = new OpenAI();
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-    let body: OpenAiRawRequestParams = req.body;
+const _handler = async (req: NextApiRequest, res: NextApiResponse) => {
+    const body: OpenAiRawRequestParams = req.body;
 
     const completion = await openai.chat.completions.create({
         model: "gpt-3.5-turbo",
